2cffaf4582384c3755cd49d4c930d783fdb41b9c
lechemindefer <dschangjuste@yahoo.fr>
2023-02-07 11:47:27 +0100

    add modified class. commit

--- src/main/java/eric/tsafack/api/config/SecurityConfig.java
+++ src/main/java/eric/tsafack/api/config/SecurityConfig.java
@@ -4,6 +4,7 @@ package eric.tsafack.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.configurers.provisioning.InMemoryUserDetailsManagerConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
